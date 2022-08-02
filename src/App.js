f83d4cd83d8c30ef74a8939abd6385af62a7ed79
kuma <kumagithub@mail.ru>
2022-08-02 17:19:17 +0600

    delete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Comment from "./components/Comment";
+import {comment} from './data/data'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Comment
+        author={comment.author}
+        text={comment.text}
+        date={comment.date}
+      />
     </div>
   );
 }
 
 export default App;
+
